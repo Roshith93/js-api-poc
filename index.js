4da48ad5430942edaa1bb5a33c64b64fbf07aa42
Roshith R <r.roshith@gmail.com>
2021-06-01 12:48:42 +0530

    Fetching the data for changes

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-import 'regenerator-runtime/runtime';
-import axios from 'axios';
+import 'regenerator-runtime/runtime'
+import axios from 'axios'
+import { tempData } from './temp'
 
-const BASE_URL = 'https://temparray.free.beeceptor.com';
+const BASE_URL = 'http://localhost:3000/historyData'
 
-const getfirstArray = async () => {
+var arrayValues = JSON.parse(localStorage.getItem('oldArrayValues')) || []
+var oldArrayValues = []
+var newArrayValues = []
+
+// != get initial data First time
+const getArray = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/firstArray`);
-
-    const todoItems = response.data;
-
-    console.log( todoItems);
-
-    return todoItems;
+    const response = await axios.get(`${BASE_URL}`)
+    return response.data
   } catch (errors) {
-    console.error(errors);
+    console.error(errors)
   }
-};
+}
+getArray().then((res) => {
+  localStorage.setItem('oldArrayValues', JSON.stringify(res))
+  oldArrayValues = JSON.parse(localStorage.getItem('oldArrayValues'))
+})
+setInterval(() => {
+  getArray().then((res) => {
+    localStorage.setItem('newArrayValues', JSON.stringify(res))
+    newArrayValues = JSON.parse(localStorage.getItem('newArrayValues'))
+    // == get status 
+    var oldArrayStatusValues = oldArrayValues.map(({ status }) => status)
+    var newArrayStatusValues = newArrayValues.map(({ status }) => status)
+    //  == find difference
+   let difference = oldArrayStatusValues.filter(x => !newArrayStatusValues.includes(x)).concat(newArrayStatusValues.filter(x => !oldArrayStatusValues.includes(x)));
 
-getfirstArray()
+    console.log(oldArrayStatusValues, newArrayStatusValues)
+    console.log(difference.length ? 'differnce is there' : 'difference not there')
+    if(difference.length){
+     console.log("show toast")
+     localStorage.setItem('oldArrayValues', JSON.stringify(newArrayValues))
+     console.log(oldArrayStatusValues)
+     
+    }
+  })
+}, 10000)
+
